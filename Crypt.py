0f376d612726f5db5804d8210b9d2db2f37c48f3
RMChannel <98057423+RMChannel@users.noreply.github.com>
2023-04-06 23:24:37 +0200

    Ora l'immagine ha anche una larghezza,
    Le immagini generate dalla funzione crypt ora generano immagini di dimensioni normali (non 1xN), così facendo sembra ancor di più un'immagine reale.
    
    Per farlo calcolo i numeri primi della lunghezza di uno dei 2 array di dati e poi faccio il prodotto della prima metà, così facendo l'immagine può essere divisa in una matrice di dimensioni eguali a quella della lunghezza dell'array di dati originale

--- Crypt.py
+++ Crypt.py
@@ -1,7 +1,27 @@
 from PIL import Image
 import numpy as np
 import os
-path='1980089.jpeg'
+def findprimenumbers(n):
+    listnumber=[]
+    i=2
+    while i<n:
+        if n%i==0:
+            listnumber.append(i)
+            n/=i
+            i=2
+        else: i+=1
+        if i==n:
+            listnumber.append(i)
+    if len(listnumber)%2==0: middle=int((len(listnumber))/2)
+    else: middle=int((len(listnumber)-1)/2)
+    i=0
+    n=1
+    while i<middle:
+        n=n*listnumber[i]
+        i+=1
+    return n
+
+path='a.png'
 extension=os.path.splitext(path)
 extension=extension[1]
 file=open(path,'rb')
@@ -17,17 +37,10 @@ extension=list(extension.encode())
 extension.append(len(extension))
 datab+=extension
 datac+=extension
-a=len(datab)-1
-while a>0:
-    if len(datab)%a==0:
-        print(a)
-    a-=1
-print(a)
-datab=np.array_split(datab, 2)
-print(datab)
-arrayb = np.array(datab, dtype=np.int32)
-arrayc = np.array(datac, dtype=np.int32)
-imgb=Image.fromarray(arrayb)
-imgc=Image.fromarray(arrayc)
+lenn=findprimenumbers(len(datab))
+datab=np.matrix(np.array_split(datab,lenn))
+datac=np.matrix(np.array_split(datac,lenn))
+imgb=Image.fromarray(datab)
+imgc=Image.fromarray(datac)
 imgb.save('b.png')
 imgc.save('c.png')
--- cryptbyrc.py
+++ cryptbyrc.py
@@ -2,6 +2,26 @@ from PIL import Image
 import numpy as np
 import os
 
+def findprimenumbers(n):
+    listnumber=[]
+    i=2
+    while i<n:
+        if n%i==0:
+            listnumber.append(i)
+            n/=i
+            i=2
+        else: i+=1
+        if i==n:
+            listnumber.append(i)
+    if len(listnumber)%2==0: middle=int((len(listnumber))/2)
+    else: middle=int((len(listnumber)-1)/2)
+    i=0
+    n=1
+    while i<middle:
+        n=n*listnumber[i]
+        i+=1
+    return n
+
 def decrypt(pathb,pathc):
     file=open('decrypt','wb')
     file2=Image.open(pathb,'r')
@@ -52,9 +72,10 @@ def crypt(path):
     extension.append(len(extension))
     datab+=extension
     datac+=extension
-    arrayb = np.array(datab, dtype=np.int32)
-    arrayc = np.array(datac, dtype=np.int32)
-    imgb=Image.fromarray(arrayb)
-    imgc=Image.fromarray(arrayc)
+    lenn=findprimenumbers(len(datab))
+    datab=np.matrix(np.array_split(datab,lenn))
+    datac=np.matrix(np.array_split(datac,lenn))
+    imgb=Image.fromarray(datab)
+    imgc=Image.fromarray(datac)
     imgb.save('b.png')
     imgc.save('c.png')
